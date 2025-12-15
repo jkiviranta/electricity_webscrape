e63ee3c503c4d6100bc89eabc45f6016f7ba258e
Juuso Kiviranta <124182914+jkiviranta@users.noreply.github.com>
2025-12-15 11:25:26 +0100

    Update swedish_contracts_se4_linux.py

--- swedish_contracts_se4_linux.py
+++ swedish_contracts_se4_linux.py
@@ -141,9 +141,14 @@ def open_page():
     options = webdriver.ChromeOptions()
     
     # Set options
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
+    #options.add_argument("--headless")
+    #options.add_argument("--disable-gpu")
     #options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
     
     # Initialize the driver with specified options
     driver = webdriver.Chrome(service=service, options=options)    
