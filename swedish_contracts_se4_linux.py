56f71abbc59a55b83e36c920073f8db2a67b1e90
Juuso Kiviranta <124182914+jkiviranta@users.noreply.github.com>
2025-12-15 11:14:03 +0100

    Update Chrome options for headless mode

--- swedish_contracts_se4_linux.py
+++ swedish_contracts_se4_linux.py
@@ -141,9 +141,8 @@ def open_page():
     options = webdriver.ChromeOptions()
     
     # Set options
-    #options.add_argument("--headless")
-    #options.add_argument("--disable-gpu")
-    options.add_argument("--start-maximized")
+    options.add_argument("--headless=new")
+    options.add_argument("--disable-gpu")
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     
     # Initialize the driver with specified options
